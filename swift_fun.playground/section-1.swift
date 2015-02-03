2823671477b7f39da12d8316a5a179a98df96f4d
d3QUone <volkvid@yandex.ru>
2015-02-03 20:43:34 +0300

    1st attempt to design a eq-builder added test structure added solving structure

--- swift_fun.playground/section-1.swift
+++ swift_fun.playground/section-1.swift
@@ -1,3 +1,4 @@
+import Darwin
 import UIKit
 println("Working with functions\n")
 
@@ -39,3 +40,60 @@ for i in 1..<10 {
     out.append(j)
 }
 println(out)
+
+var x = [[1, 2], [2, 3, 5]]
+
+println("\nTrying to work with interpretering math equals")
+
+// creates random math equals
+func create_test_cases(amount: Int) -> [String]{
+    /*
+    let ava_Operators = ["(", ")", "+", "-", "*", "/"]
+    let ava_Consts = ["PI", "E"]
+    let ava_Funcs = ["cos", "sin", "exp"]
+    */
+    let available = [["cos", "sin", "exp"], ["(", ")", "+", "-", "*", "/"], ["PI", "E"]]
+    
+    var res = [String]()
+    for i in 0..<amount {
+        var s = ""
+        let len: Int = Int(arc4random_uniform(10)+1)*2
+        for j in 0..<len {
+            let group_num: Int = Int(arc4random_uniform(3))
+            let group_len: Int = countElements(available[group_num])
+            let item_num: Int = Int(arc4random_uniform(UInt32(group_len)))
+            
+            let item = available[group_num][item_num]
+            var spaces: String = ""
+            for sp in 0..<group_num {
+                spaces += " "
+            }
+            
+            if group_num == 0 {
+                s += item + spaces + "(" + spaces
+            }
+            else {
+                let num: String = String(arc4random_uniform(100))
+                s += item + spaces + num + " "
+            }
+        }
+        res.append(s)
+    }
+    return res
+}
+
+func do_calc(input: String) -> Float {
+    var out: Float = 0.0
+    // ???
+    var brackets: Int = 0
+    
+    return out
+}
+
+// do tests...
+var math_input = create_test_cases(10)
+for test_case in math_input {
+    println(do_calc(test_case))
+}
+
+
