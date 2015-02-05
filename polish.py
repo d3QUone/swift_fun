a780b102c9b3945c47db20774f21db94d76d2eb0
d3QUone <volkvid@yandex.ru>
2015-02-05 15:31:24 +0300

    added functions in count RPN (not in parsing yet)

--- polish.py
+++ polish.py
@@ -2,14 +2,14 @@ import math
 
 # ---- test func ----
 def test_stuff(name, function, cases):
-    print "Doing tests of {0}...".format(name)
+    print "Doing tests of {0}...\n".format(name)
     for case in cases:
         # case[i] = [eq, ans]
         res = function(case[0])
         if abs(float(res) - float(case[1])) < 0.001:
-            print "OK: {0} = {1}".format(case[0], case[1])
+            print "-OK: {0} = {1}".format(case[0], case[1])
         else:
-            print "Error: {0} != {1}".format(res, case[1])
+            print "---Error: {0} != {1}".format(res, case[1])
         print " "
 
 
@@ -31,7 +31,10 @@ def count(a_list):
     exp = list(a_list)
     i = 0
     while len(exp) > 1:
-        item = exp[i]
+        try:
+            item = exp[i]
+        except:
+            return "ii-e" #"Incorrect input"
         if item in operators:
             try:
                 a = float(exp[i-2])
@@ -52,18 +55,28 @@ def count(a_list):
                 i = 0
             except:
                 print "ex", item
-                return "Incorrect operator"
+                return "io-e" #"Incorrect operator"
         elif item in functions:
             try:
                 a = float(exp[i-1])
-                # calcs ...
-                res = "func-calc"
+                if item == "sin":
+                    res = math.sin(a)
+                elif item == "cos":
+                    res = math.cos(a)
+                elif item == "tan":
+                    res = math.tan(a)
+                elif item == "exp":
+                    res = math.exp(a)
+                elif item == "ln":
+                    res = math.log(a, math.e)
+                elif item == "log":
+                    res = math.log(a, 10)
                 exp[i-1] = res
                 exp.pop(i)
                 i = 0
             except:
                 print "ex", item
-                return "Incorrect function"
+                return "if-e" #"Incorrect function"
         else:
             i += 1
     return str(exp[0])
@@ -98,8 +111,8 @@ def creplace(simple_input):
             if proc != "":
                 append(proc)
                 proc = ""
-            # magic goes below
-            if len(stack) == 0: stack += item
+            if len(stack) == 0:
+                stack += item
             else:
                 try:
                     # do it on the whole stack
@@ -108,28 +121,27 @@ def creplace(simple_input):
                         a = int(operator_power(stack[j]))
                         b = int(operator_power(item))
                         if a >= b:
-                            print "pop stack({0}): {1}".format(j, stack[j])
+                            #print "pop stack({0}): {1}".format(j, stack[j])
                             append(stack[j])
                             stack = stack[:j] + stack[j+1:]
-                        else:
-                            print "push to stack({0}): {1}".format(j, item) 
+                        #else:
+                            #print "push to stack({0}): {1}".format(j, item) 
                     stack += item
                 except Exception as e:
                     print "operator-exception:", e, "item:", item
-                    return "operator error"
+                    return "oe"
         else:
-            return "error"
+            return "ee"
         i += 1
     # add reversed stack to output
     i = len(stack) - 1
-    print "len =", len(stack)
     while i >= 0:
         append(stack[i])
         i -= 1
-    print "out:", out
+    #print "out:", out
     return out
 
-
+'''
 cases3 = [["4^2", "16"], ["41+57", "98"], ["2*4", "8"], ["9+0-3", "6"], ["2*3-1", "5"], ["12/3+1", "5"],
           ["12+3*41-33", "102"], ["40/8+10/10-2", "4"], ["2*3*4-5*6", "-6"], ["30-5/2+1", "28.5"]]
 print "Doing tests of CREPLACE...."
@@ -141,4 +153,53 @@ for case in cases3:
     else:
         print "Error: {0} != {1}".format(res, case[1])
     print " "
+'''        
+
+
+def any_expression(exp):
+    exp = exp.replace(" ", "")
+    print "inp: " + exp + "; len =", len(exp), "..."
+    sub_exp = "" # a top-bracket exp 
+    i = 0
+    a = -1
+    b = -1
+    brackets = 0
+    # this must simplyfy input to no-brackets expression
+    while i < len(exp): #or len(exp) > 0
+        if exp[0] == "(" and exp[len(exp)-1] == ")":
+            exp = exp[1:-1]
+            i = 0
+            print "updated exp to", exp # just not to waste time
+        char = exp[i]
+        if char == "(":
+            a = i
+            brackets -= 1
+        elif char == ")":
+            b = i
+            brackets += 1
+            if a != -1 and b > a:
+                # do calcs, replace by result:
+                sub_exp = exp[a+1:b] 
+                sub_RPN = creplace(sub_exp)
+                if sub_RPN in ["ii-e", "io-e", "if-e"]:
+                    return sub_RPN
+                else:
+                    sub_result = count(sub_RPN)
+                exp = exp[:a] + sub_result + exp[b+1:]
+                print "sub:", sub_exp, "| full:", exp, " | b =", brackets
+                i = -1 # cause +1 at the end -> i will be at the beginning 0
+                brackets = 0 # cause starting from the beginning now
+                a = -1
+                b = -1
+            else:
+                print "i = {0}, smth wrong".format(i) # wrong input as usual
+        i += 1
+    sub_RPN = creplace(exp)
+    exp = count(sub_RPN)
+    print "finaly total simplify:", exp, ", brackets-balance:", brackets, "\n"
+    return brackets
         
+
+cases4 = [["1+(2+(3+4*(5+6)))", 0], ["(((1-2)-3)-4)-5", 0], ["(1 +3/(45* (33/44)-5))", 0], ["30/(3+50)-11*(21-44/11)", 0],
+          ["(30/(3+50)-11*(21-44/11))", 0], ["(30/(3+50)-11*(21-44/11)", -1]]
+test_stuff("MONSTER CALC", any_expression, cases4)
